98166439c3f1713d2f6c08cee1905489c89e8998
Adam Gerbens <adam.gerbens@gmail.com>
2020-07-20 23:43:30 -0700

    Remove pointless file

--- src/hooks/index.js
+++
@@ -1,9 +0,0 @@
-import useDarkMode from './useDarkMode';
-import useLocalStorage from './useLocalStorage';
-import useMedia from './useMedia';
-
-export default {
-  useDarkMode,
-  useLocalStorage,
-  useMedia,
-};
